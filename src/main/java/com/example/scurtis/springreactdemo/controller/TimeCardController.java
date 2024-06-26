0d9d7d128b1fe394a8da43e7f3e87f7c7fbbf56e
Sean Curtis <sean_curtis@discovery.com>
2024-06-26 13:47:44 -0400

    make TimeCardController actually return something, add TimeCardRepository mock to test, add test config yaml

--- src/main/java/com/example/scurtis/springreactdemo/controller/TimeCardController.java
+++ src/main/java/com/example/scurtis/springreactdemo/controller/TimeCardController.java
@@ -1,15 +1,29 @@
 package com.example.scurtis.springreactdemo.controller;
 
+import com.example.scurtis.springreactdemo.repository.TimeCardRepository;
+import com.example.scurtis.springreactdemo.timekeeping.TimeCard;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.UUID;
+
 @RestController
 public class TimeCardController {
 
-    @GetMapping("/timecards/{timecardId}")
-    private ResponseEntity<?> findById(@PathVariable Long timecardId) {
-        return null;
+    private final TimeCardRepository timeCardRepository;
+
+    private TimeCardController(TimeCardRepository timeCardRepository) {
+        this.timeCardRepository = timeCardRepository;
     }
+
+    @GetMapping("/timecards/{timecardId}")
+    private ResponseEntity<?> findById(@PathVariable UUID timecardId) {
+        Optional<TimeCard> timeCardOptional = timeCardRepository.findById(timecardId);
+        return timeCardOptional.isPresent() ? ResponseEntity.ok(timeCardOptional.get()) : ResponseEntity.notFound().build();
+    }
+
 }
--- src/test/java/com/example/scurtis/springreactdemo/controller/TimeCardControllerTest.java
+++ src/test/java/com/example/scurtis/springreactdemo/controller/TimeCardControllerTest.java
@@ -1,9 +1,17 @@
 package com.example.scurtis.springreactdemo.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.scurtis.springreactdemo.repository.TimeCardRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.MockBeans;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,9 +20,17 @@ import static org.junit.jupiter.api.Assertions.*;
         controllers = TimeCardController.class,
         properties = {}
 )
+@MockBean(
+    value= {
+        TimeCardRepository.class
+    }
+)
 @AutoConfigureWebClient
 @ActiveProfiles("test")
 class TimeCardControllerTest {
+
+    @Autowired
+    private TimeCardController timeCardController;
 
     @BeforeEach
     void setUp() {
@@ -22,5 +38,10 @@ class TimeCardControllerTest {
 
     @AfterEach
     void tearDown() {
+    }
+
+    @Test
+    void contextLoads() {
+        assertThat(timeCardController).isNotNull();
     }
 }
